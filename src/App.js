af8da8382e11fe64535d6406b9d1754ea461362b
Jack Gay <jackpowis@gmail.com>
2019-08-05 20:11:57 +0100

    working on styling

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p className="first-para">I am a London based interdisciplinary graphic designer, working predominantly within the arts, music and fashion. Clients include, Oval Space, The Pickle Factory, Rollacoaster Magazine, Stella McCartney, University College of London and Rinse FM.</p>
-      <p>For enquiries, contact me via email, or see a selection of work on my Instagram.</p>
+      <p className="first-para">I am a London based interdisciplinary graphic designer, working predominantly within the arts, music and fashion. Clients include,<span className="sans-style"> Oval Space, The Pickle Factory, Rollacoaster Magazine, Stella McCartney, University College of London</span> and <span className="sans-style">Rinse FM</span>.</p>
+      <p>For enquiries, contact me via <span className="underline-word">email</span>, or see a selection of work on my <span className="underline-word">Instagram</span>.</p>
     </div>
   );
 }
