2494655964cf4f27668ac202be2873602a6eaa5b
Jack Gay <jackpowis@gmail.com>
2019-09-26 18:14:25 +0100

    creating responsiveness

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <p className="first-para">I am a London based interdisciplinary graphic designer, working predominantly within the arts, music and fashion. Clients include,<span className="sans-style"> Oval Space, The Pickle Factory, Rollacoaster Magazine, Stella McCartney, University College of London</span> and <span className="sans-style">Rinse FM</span>.</p>
-      <p>For enquiries, contact me via <span className="underline-word"><a href = "mailto: bene.pooley@gmail.com" target="blank">email</a></span>, or see a selection of work on my <span className="underline-word"><a href="https://www.instagram.com/bene.pooley/" target="blank">Instagram</a></span>.</p>
+      <p className= "second-para">For enquiries, contact me via <span className="underline-word"><a href = "mailto: bene.pooley@gmail.com" target="blank">email</a></span>, or see a selection of work on my <span className="underline-word"><a href="https://www.instagram.com/bene.pooley/" target="blank">Instagram</a></span>.</p>
     </div>
   );
 }
